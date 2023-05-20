bd04221937c8ba62384e667801898e6465e62ec6
mau <mau.eth.auditor@gmail.com>
2023-05-20 13:38:22 -0300

    grim finance

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,26 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config({ path: __dirname + '/.env' });
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.18",
-};
+const MAINNET_FORK_BLOCK_NUMBER = 25345000;
+
+const COMPILERS = [
+  {
+    version: '0.8.0',
+  }
+];
+
+('hardhat/config').HardhatUserConfig 
+console.log(`Forking Mainnet Block Height ${MAINNET_FORK_BLOCK_NUMBER}`);
+  module.exports = {
+    networks: {
+      hardhat: {
+        forking: {
+          url: process.env.FANTOM,
+          blockNumber: MAINNET_FORK_BLOCK_NUMBER,
+        },
+      },
+    },
+    solidity: {
+      compilers: COMPILERS,
+    },
+  };
